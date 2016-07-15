ea1f7c438674a5b02544408953bc85beb182c3c7
Julius Ware <jcware8@gmail.com>
2016-07-15 13:22:36 -0400

    adding handlebars, set up routing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var expressHbs = require('express-handlebars');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,10 +10,12 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('handlebars', expressHbs({defaultLayout:'main.handlebars'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -20,10 +23,38 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+//routing using handlebars
+app.get('/', function(req, res){
+    res.render('index');
+
+});
+
+app.get('/about', function(req, res){
+    res.render('about');
+});
+
+
+
+//app.use('/', routes);
+//app.use('/users', users);
+
+//route to home page
+/*router.get('/', function(req,res){
+    res.send("i'm the home page ");
+
+});*/
+
+//route to about page
+/*router.get('/about', function(req, res){
+    res.send("i'm the about page");
+
+});*/
+
+//app.use('/', router);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,10 +70,7 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    res.render('error');
   });
 }
 
