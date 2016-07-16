490b3d1a52ec7d042081d601e10c0aa42542ee89
Julius Ware <jcware8@gmail.com>
2016-07-15 21:33:44 -0400

    got oauth working, now successfully redirects and renders app page. Need to figure out debugging and data access.

---
+++ public/js/app_page.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('materialboxed').materialbox();
+    $('.carousel').carousel({padding: 50});
+    $('.user').pushpin();
+    //$('.carousel.carousel-slider').carousel({full_width: true});
+});
